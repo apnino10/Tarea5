7e42289cbaa0b25cbb0c7a74f71c001487e31ff9
Andrea_Niño <32989368+apnino10@users.noreply.github.com>
2017-12-03 10:07:20 -0500

    Add files via upload

--- CurvaRotacion.c
+++ CurvaRotacion.c
@@ -3,18 +3,19 @@
 #include <math.h>
 
 int main(){
-
 	
-	
-	/*el numero para saber el tamaño del puntero y creandolos*/
+	/*el numero para saber el tamaño del puntero y creandolos, definiendo las variables*/
 	int i = 300;
 	int n = 300; 
 	int contador = 300;
+	int numit = 100000;
+	double delta = 0.5;
 
 	double radio, velocidad, se_parece_a;
+	double Mb_viejo, Mb_nuevo, Md_viejo, Md_nuevo, Mh_viejo, Mh_nuevo, alpha;
 
-	double *RADIO=malloc(n*sizeof(double));
-    	double *VELOCIDAD=malloc(n*sizeof(double));
+	double *RADIO = malloc(n*sizeof(double));
+    	double *VELOCIDAD = malloc(n*sizeof(double));
 	
 	/*Leyendo el archivo*/
 	/*Leyendo las 2 columnas*/	
@@ -31,17 +32,36 @@ int main(){
 	/*Creando el pointer donde se guardaran los nuevos valores de velocidad calculados*/
 	/*Al graficar los datos del archivo, la tendencia general de estos es similar a e^-x*/
 	/*Generando los numeros aleatorios para calcular los parametros*/
+	/*alpha permite saber cuando se abanza, si alpha > 1 no sirve y hay que buscar otro*/
 
-	double *VELOCIDAD_2=malloc(n*sizeof(double));
-	double Mb, Md, Mh;
+	double *VELOCIDAD_2 = malloc(n*sizeof(double));
 	double *SE_PARECE_A = malloc(n*sizeof(double));
 
-	for( i=0; i< n; i++ ) {
+	Mb_viejo = 2.0*drand48() - 1.0;	
+	Md_viejo = 2.0*drand48() - 1.0;
+	Mh_viejo = 2.0*drand48() - 1.0;
+
+	for( i=0; i<numit; i++ ) {
+
+		Mb_nuevo = Mb_viejo + (2.0*drand48() - 1.0);	
+		Md_nuevo = Md_viejo + (2.0*drand48() - 1.0);
+		Mh_nuevo = Mh_viejo + (2.0*drand48() - 1.0);
+
+		if(alpha > 1)
+		{
+		    alpha = 1;
+		}
+		if(alpha > drandom48())
+		{
+		    Mb_nuevo = Mb_viejo;
+		    Md_nuevo = Md_viejo;
+		    Mh_nuevo = Mh_viejo;
+		}
+
+
 
 		se_parece_a = exp(-1*radio);
-		Mb=drand48();	
-		Md=drand48();
-		Mh=drand48();
+		
 
 		RADIO[i] = radio;	
 		VELOCIDAD[i] = velocidad;	
@@ -49,10 +69,16 @@ int main(){
 
 	}
 
+	 velocidad_2[i]= pow
+
+((((Mb)**(1/2))*radio[i])/((radio[i]+bb**2)**(3/4)))+((((Md)**(1/2))*radio[i])/(((radio[i])**2 + (bd +ad)**2)**(3/4)))+(((Mh)**(1/2))/((((radio[i])**2) + ah**2)**(1/4)))
+
+	
+
 	/*Guardando los valores en un txt e imprimiendo en la consola*/
 	
 	FILE *modelo;
-	modelo=fopen("Resultados_hw5.txt", "w");
+	modelo = fopen("Resultados_hw5.txt", "w");
 	for(i=0; i<n; i++){
         	fprintf(modelo, "%f %f %f \n", Mb, Md, Mh);
 	}
--- Tarea5.mk
+++ Tarea5.mk
@@ -1,5 +1,7 @@
-Results_hw5.pdf : Resultados_hw5.txt Plots.py
-	python Plots.py
+image_targets = Results_hw5.pdf
+
+Results_hw5.pdf : Resultados_hw5.tex Plots.py
+	pdflatex $< && python Plots.py
 
 Resultados_hw5.txt : curva.x
 	./curva.x
